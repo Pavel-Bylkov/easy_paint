d3cf3c054576da2c5d71ca68cb072efc6e64ff8d
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-03-25 19:26:11 +0300

    update

--- paint.py
+++ paint.py
@@ -6,6 +6,9 @@ class Paint(Frame):
         super().__init__(parent)
         self.parent = parent
         self.setGUI()
+        self.brush_size = 10
+        self.brush_color = "black"
+        self.canv.bind("<B1-Motion>", self.draw)
 
     def setGUI(self):
         self.parent.title("Easy Paint")
@@ -14,12 +17,12 @@ class Paint(Frame):
         self.canv = Canvas(self, bg="white")  # Создаем поле для рисования, устанавливаем белый фон
 
         color_lab = Label(self, text="Color: ")  # Создаем метку для кнопок изменения цвета кисти
-        red_btn = Button(self, text="Red", width=10)  # Создание кнопки:  Установка текста кнопки,
+        red_btn = Button(self, text="Red", width=10, bg='red', fg='black')  # Создание кнопки:  Установка текста кнопки,
         # задание ширины кнопки (10 символов)
-        green_btn = Button(self, text="Green", width=10)
-        blue_btn = Button(self, text="Blue", width=10)
-        black_btn = Button(self, text="Black", width=10)
-        white_btn = Button(self, text="White", width=10)
+        green_btn = Button(self, text="Green", width=10, bg='green', fg='black')
+        blue_btn = Button(self, text="Blue", width=10, bg='blue', fg='black')
+        black_btn = Button(self, text="Black", width=10, bg='black', fg='black')
+        white_btn = Button(self, text="White", width=10, bg='white', fg='black')
 
         size_lab = Label(self, text="Brush size: ")  # Создаем метку для кнопок изменения размера кисти
         btn_1 = Button(self, text="Size 1", width=10)
@@ -54,6 +57,13 @@ class Paint(Frame):
         btn_5.grid(row=1, column=5)
         btn_6.grid(row=1, column=6)
         btn_7.grid(row=1, column=7, sticky=W)
+
+    def draw(self, event):
+        self.canv.create_oval(event.x - self.brush_size,
+                              event.y - self.brush_size,
+                              event.x + self.brush_size,
+                              event.y + self.brush_size,
+                              fill=self.brush_color, outline=self.brush_color)
 
 
 def main():
